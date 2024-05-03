09f37caa694104c4aed4b1f9323343e0ea4d3fa9
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-05-03 11:54:33 +0100

    working on admin middlewalre

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,37 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { isValidPassword } from "./lib/isValidPassword";
 
-export async function middleware(req: NextRequest) {}
+export async function middleware(req: NextRequest) {
+  if ((await isAuthenticated(req)) === false) {
+    return new NextResponse("Unauthorized", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": "Basic",
+      },
+    });
+  }
+}
+
+async function isAuthenticated(req: NextRequest) {
+  const authHeader =
+    req.headers.get("authorization") || req.headers.get("Authorization");
+
+  if (authHeader == null) return false;
+
+  const [userName, password] = Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  console.log(userName, password);
+
+  return (
+    userName === process.env.ADMIN_USERNAME &&
+    // (await isValidPassword(
+    //   password,
+    //   process.env.HASHED_ADMIN_PASSWORD as string
+    // ))
+    console.log(process.env.HASHED_ADMIN_PASSWORD as string)
+  );
+}
 
 export const config = {
   matcher: "/admin/:path*",
