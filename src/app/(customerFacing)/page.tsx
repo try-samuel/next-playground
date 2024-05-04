f1b77bce8c6fbc18ca2a326ddb152812d885fc78
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-05-04 20:31:53 +0100

    ProductSuspense wants to injure me

---
+++ src/app/(customerFacing)/page.tsx
@@ -0,0 +1,84 @@
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
+import { Button } from "@/components/ui/button";
+import db from "@/db/db";
+import { Product } from "@prisma/client";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import { Suspense } from "react";
+
+function getPopularProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: {
+      Orders: { _count: "desc" },
+    },
+    take: 6,
+  });
+}
+
+function getNewestProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { createdAt: "desc" },
+    take: 6,
+  });
+}
+
+export default function HomePage() {
+  return (
+    <main className="space-y-12">
+      <ProductGridSection
+        title="Most Popular"
+        productsFetcher={getPopularProducts}
+      />
+      <ProductGridSection title="Newest" productsFetcher={getNewestProducts} />
+    </main>
+  );
+}
+
+type ProductGridSectionprops = {
+  productsFetcher: () => Promise<Product[]>;
+  title: string;
+};
+
+function ProductGridSection({
+  productsFetcher,
+  title,
+}: ProductGridSectionprops) {
+  return (
+    <section className="space-y-4">
+      <div className="flex gap-4">
+        <h2 className="text-3xl font-black">{title}</h2>
+        <Button asChild variant="outline">
+          <Link href="/products" className="space-x-2">
+            <span>View All</span>
+            <ArrowRight className="size-4" />
+          </Link>
+        </Button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Suspense
+          fallback={
+            <>
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+            </>
+          }
+        >
+          <ProductSuspense productsFetcher={productsFetcher} />
+        </Suspense>
+      </div>
+    </section>
+  );
+}
+
+async function ProductSuspense({
+  productsFetcher,
+}: {
+  productsFetcher: () => Promise<Product[]>;
+}) {
+  return (await productsFetcher()).map((product) => (
+    <ProductCard key={product.id} {...product} />
+  ));
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,32 @@
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
+import db from "@/db/db";
+import { Suspense } from "react";
+
+function getProducts() {
+  return db.product.findMany({ where: { isAvailableForPurchase: true } });
+}
+
+export function ProductsPage() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Suspense
+        fallback={
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        }
+      >
+        <ProductSuspense productsFetcher={productsFetcher} />
+      </Suspense>
+    </div>
+  );
+}
+
+async function ProductSuspense() {
+  const products = getProducts();
+  return (await productsFetcher()).map((product) => (
+    <ProductCard key={product.id} {...product} />
+  ));
+}
