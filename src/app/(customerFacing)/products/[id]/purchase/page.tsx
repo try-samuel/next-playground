76474e5cf70b556fb587bc5c2c62d21608cc8d4e
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-05-09 22:48:15 +0100

    stopping here, stripe is anti Nigeria

---
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -0,0 +1,17 @@
+import db from "@/db/db";
+import { notFound } from "next/navigation";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function PurchasePage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = await db.product.findUnique({ where: { id } });
+  if (product == null) {
+    return notFound();
+  }
+  return <h1>Hi</h1>;
+}
